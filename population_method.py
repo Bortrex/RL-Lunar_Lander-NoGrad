2af74a6e43c7d19e10150ae3a5ccbae0d793fef3
scastillo <scastillo.lenz@gmail.com>
2026-09-23 18:33:47 +0200

    fixes

--- population_method.py
+++ population_method.py
@@ -67,7 +67,7 @@ class Policy(nn.Module):
         return self.net(x)
 
 @torch.no_grad()
-def evaluate(policy, episode_seeds=None):
+def evaluate(policy, episode_seeds):
     ''' Evaluate the policy over a number of episodes and return the smoothed reward. 
     
         Returns: `smoothed_reward` 
@@ -136,7 +136,6 @@ SIGMA = 0.1             # Standard deviation for perturbing weights
 CHILD_EPIS = 5          # number of evals per child
 
 
-
 # Setting seed
 rng = np.random.default_rng(SEED)  # generator
 torch.cuda.empty_cache()
@@ -146,57 +145,60 @@ if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-policy = Policy(env).to(device) # setting main agent
-# each child inits with random weights
-population = [Policy(env).to(device) for _ in range(POP_SIZE)]
+def main():
+
+    policy = Policy(env).to(device) # setting main agent
+    # each child inits with random weights
+    population = [Policy(env).to(device) for _ in range(POP_SIZE)]
 
 
-output_dir = Path("dataPlots/")
-output_dir.mkdir(parents=True, exist_ok=True)
-f = open(f"dataPlots/dataPopu-{SEED}.dat", "w")
-print("RETURN", f"population-{SEED}", file=f)
+    output_dir = Path("dataPlots/")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    f = open(f"dataPlots/dataPopu-{SEED}.dat", "w")
+    print("RETURN", f"population-{SEED}", file=f)
 
-for gen in range(NUM_GENERATIONS):
+    for gen in range(NUM_GENERATIONS):
 
-    episode_seeds = rng.integers(0, 2**31 - 1, size=CHILD_EPIS).tolist()  # episode seeds for child evaluation
-    # evaluating childs
-    rewards = [evaluate(p, episode_seeds=episode_seeds) 
-               for p in population]
-    topK = int(PARENT_FRAC * POP_SIZE)
-    parent_indices = np.argsort(rewards)[-topK:]
-    parent_policies = [population[i] for i in parent_indices]
+        episode_seeds = rng.integers(0, 2**31 - 1, size=CHILD_EPIS).tolist()  # episode seeds for child evaluation
+        # evaluating childs
+        rewards = [evaluate(p, episode_seeds=episode_seeds) 
+                for p in population]
+        topK = int(PARENT_FRAC * POP_SIZE)
+        parent_indices = np.argsort(rewards)[-topK:]
+        parent_policies = [population[i] for i in parent_indices]
+        
+
+        new_population = []
+        for _ in range(POP_SIZE):
+            parent_idx = rng.choice(topK)
+            child = Policy(env).to(device)
+            # updating child
+            with torch.no_grad():
+                child.load_state_dict(parent_policies[parent_idx].state_dict())
+                for param in child.parameters():                    
+                    param.add_(torch.randn_like(param.data) * SIGMA)
+            new_population.append(child)
+        
+        population = new_population
+
+
+        # combining all parameters from parents
+        for name, param in policy.named_parameters():
+            parent_params = [p.state_dict()[name] for p in parent_policies]
+            params_mean = torch.mean(torch.stack(parent_params), dim=0)
+            param.data.copy_(params_mean)
+        
+        evaluation_reward = evaluate_parent(policy, EVAL_SEEDS)
+                
+        if gen%4==0:
+            print(f"RETURN \tepisode: {gen+1}, \t reward: {evaluation_reward:.4f}")
+        print("RETURN", gen+1, evaluation_reward, file=f)
+        
+    print('e', file=f)
+
+    # Close the environment
+    env.close()
+    f.close()
     
-
-    new_population = []
-    for _ in range(POP_SIZE):
-        parent_idx = rng.choice(topK)
-        child = Policy(env).to(device)
-        # updating child
-        with torch.no_grad():
-            child.load_state_dict(parent_policies[parent_idx].state_dict())
-            for param in child.parameters():                    
-                param.add_(torch.randn_like(param.data) * SIGMA)
-        new_population.append(child)
-    
-    population = new_population
-
-
-    # combining all parameters from parents
-    for name, param in policy.named_parameters():
-        parent_params = [p.state_dict()[name] for p in parent_policies]
-        params_mean = torch.mean(torch.stack(parent_params), dim=0)
-        param.data.copy_(params_mean)
-    
-    evaluation_reward = evaluate_parent(policy, EVAL_SEEDS)
-            
-    if gen%4==0:
-        print(f"RETURN \tepisode: {gen+1}, \t reward: {evaluation_reward:.4f}")
-    print("RETURN", gen+1, evaluation_reward, file=f)
-    
-print('e', file=f)
-
-# Close the environment
-env.close()
-f.close()
-    
-
+if __name__ == "__main__":
+    main()
